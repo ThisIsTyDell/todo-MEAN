eb232980969c8e35b57803249bd923a617444d15
ThisIsTyDell <milleb94@gmail.com>
2018-02-17 21:47:21 -0600

    change to function

--- todoapp/routes/api/todos.route.js
+++ todoapp/routes/api/todos.route.js
@@ -5,7 +5,9 @@ var router = express.Router();
 var ToDoController = require('../../controllers/todos.controller');
 
 router.get('/', ToDoController.getTodos);
-router.post('/', ToDoController.createToDo);
+router.post('/', function(req, res) {
+  ToDoController.createToDo;
+});
 router.put('/', ToDoController.updateTodo);
 router.delete('/:id', ToDoController.removeTodo);
 
