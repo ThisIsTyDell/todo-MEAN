b1fc3bad7622fd5e5a448dde3184af1186eb0019
ThisIsTyDell <milleb94@gmail.com>
2018-02-17 21:03:32 -0600

    check query parameters, if no params, set default page to 1 and limit to 10 for pagination

--- todoapp/controllers/todos.controller.js
+++ todoapp/controllers/todos.controller.js
@@ -1,3 +1,8 @@
 var TodoService = require('../services/todos.service');
 
 _this = this;
+
+exports.getTodos = async function(req, res, next) {
+  var page = req.query.page ? req.query.page : 1;
+  var limit = req.query.limit ? req.query.limit : 10;
+}
