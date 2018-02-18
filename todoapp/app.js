34a4f2e6af5e076ae73522449b342b0abb007697
ThisIsTyDell <milleb94@gmail.com>
2018-02-17 21:34:48 -0600

    require api route module

--- todoapp/app.js
+++ todoapp/app.js
@@ -16,6 +16,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/todoapp', { useMongoClient: true })
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var api = require('./routes/api.route');
+
 var app = express();
 
 // view engine setup
