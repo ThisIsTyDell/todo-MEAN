befd9cbebee39fbee7f2ee88d9e296229961cfec
ThisIsTyDell <milleb94@gmail.com>
2018-02-17 21:23:50 -0600

    add update todo item action

--- todoapp/controllers/todos.controller.js
+++ todoapp/controllers/todos.controller.js
@@ -44,3 +44,38 @@ exports.createTodo = async function(req, res, next) {
     });
   }
 };
+
+exports.updateTodo = async function(req, res, next) {
+  if(!req.body._id) {
+    return res.status(400).json({
+      status: 400,
+      message: "Must provide id of todo item"
+    });
+  };
+
+  var id = req.body._id;
+
+  console.log(req.body);
+
+  var todo = {
+    id,
+    title: req.body.title ? req.body.title : null,
+    description: req.body.description ? req.body.description : null,
+    status: req.body.status ? req.body.status : null
+  };
+
+  try {
+    var updatedTodo = await TodoService.updateTodo(todo);
+
+    return res.status(200).json({
+      status: 200,
+      data: updatedTodo,
+      message: "Successfully updated the todo item"
+    });
+  } catch(e) {
+    return res.status(400).json({
+      status: 400,
+      message: e.message
+    });
+  }
+}
