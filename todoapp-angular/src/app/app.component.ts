c8bbde5853c1e958690850f74a47f39d120ec43c
ThisIsTyDell <milleb94@gmail.com>
2018-02-18 13:51:37 -0600

    add todo service to be injected into component when constructed

--- todoapp-angular/src/app/app.component.ts
+++ todoapp-angular/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private todoService: TodoService){}
   title = 'app';
 }
