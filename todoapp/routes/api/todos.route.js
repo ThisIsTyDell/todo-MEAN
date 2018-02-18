948b5cb2b46a760e5057fd7a91cbe711e0100f7a
ThisIsTyDell <milleb94@gmail.com>
2018-02-17 21:30:43 -0600

    add express router, and routes for to do items

--- todoapp/routes/api/todos.route.js
+++ todoapp/routes/api/todos.route.js
@@ -1,1 +1,12 @@
-todos.route.js
+var express = require('express');
+
+var router = express.Router();
+
+var ToDoController = require('../../controllers/todos.controllers');
+
+router.get('/', ToDoController.getTodos);
+router.post('/', ToDoController.createToDo);
+router.put('/', ToDoController.updateTodo);
+router.delete('/:id', ToDoController.removeTodo);
+
+module.exports = router;
