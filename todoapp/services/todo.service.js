4a72873b0b9a3629d3bb068987caf698bc1d30d2
ThisIsTyDell <milleb94@gmail.com>
2018-02-17 20:41:05 -0600

    add function to get todo list items

--- todoapp/services/todo.service.js
+++ todoapp/services/todo.service.js
@@ -1,3 +1,18 @@
 var ToDo = require('../models/todo.model');
 
 _this = this;
+
+exports.getTodos = async function(query, page, limit) {
+  var options = {
+    page,
+    limit
+  };
+
+  try {
+    var todos = await ToDo.paginate(query, options);
+
+    return todos;
+  } catch(e) {
+    throw Error('Error while Paginating ToDos');
+  }
+};
