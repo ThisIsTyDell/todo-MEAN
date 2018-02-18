1cca68fe8111ac907ac9a53e2f0696e9376e9e31
ThisIsTyDell <milleb94@gmail.com>
2018-02-18 13:23:06 -0600

    create todo service

--- todoapp-angular/src/app/todo.service.ts
+++ todoapp-angular/src/app/todo.service.ts
@@ -3,3 +3,11 @@ import { Observable } from 'rxjs/Rx';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Response } from '@angular/http';
 import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TodoService {
+  api_url = 'http://localhost:3000';
+  todoUrl = `${this.api_url}/api/todos`;
+
+  constructor(private http: HttpClient) {}
+}
