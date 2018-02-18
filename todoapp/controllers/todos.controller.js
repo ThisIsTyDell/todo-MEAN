25a91c626dcfc65c66d0332afe8869375b6629fc
ThisIsTyDell <milleb94@gmail.com>
2018-02-17 21:06:05 -0600

    set up get todo action

--- todoapp/controllers/todos.controller.js
+++ todoapp/controllers/todos.controller.js
@@ -5,4 +5,19 @@ _this = this;
 exports.getTodos = async function(req, res, next) {
   var page = req.query.page ? req.query.page : 1;
   var limit = req.query.limit ? req.query.limit : 10;
+
+  try {
+    var todos = await TodoService.getTodos({}, page, limit);
+
+    return res.status(200).json({
+      status: 200,
+      data: todos,
+      message: "ToDo List Recieved Successfully"
+    });
+  } catch(e) {
+    return res.status(400).json({
+      status: 400,
+      message: e.message
+    });
+  }
 }
