1fc0a13a7c21f4e55de817007522bae750049ca0
ThisIsTyDell <milleb94@gmail.com>
2018-02-18 13:40:39 -0600

    add action to create todo

--- todoapp-angular/src/app/services/todo.service.ts
+++ todoapp-angular/src/app/services/todo.service.ts
@@ -4,10 +4,16 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class TodoService {
   api_url = 'http://localhost:3000';
   todoUrl = `${this.api_url}/api/todos`;
 
   constructor(private http: HttpClient) {}
+
+  createTodo(todo: ToDo): Observable<any>{
+    return this.http.post(`${this.todoUrl}`, todo);
+  }
 }
