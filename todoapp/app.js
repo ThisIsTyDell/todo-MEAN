dd35d5678632d5e4f5c37a34a1e87c8f80f5d392
ThisIsTyDell <milleb94@gmail.com>
2018-02-17 20:24:22 -0600

    configure CORS to allow request from Angular on port 4200

--- todoapp/app.js
+++ todoapp/app.js
@@ -24,6 +24,12 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
